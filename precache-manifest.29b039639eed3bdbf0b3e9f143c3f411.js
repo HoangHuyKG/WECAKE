86be9a96af2dbe8660a0874df1e3780a33102f48
unknown <hoanghuywww10@gmail.com>
2022-02-25 09:30:06 +0700

    Updates

---
+++ precache-manifest.29b039639eed3bdbf0b3e9f143c3f411.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6f8fe606dffe62ace4142eb016f9636",
+    "url": "/WECAKE/index.html"
+  },
+  {
+    "revision": "fc5f3d445f36f26ae69b",
+    "url": "/WECAKE/static/css/main.fc5dfaa2.chunk.css"
+  },
+  {
+    "revision": "55907a7cb973491e0047",
+    "url": "/WECAKE/static/js/2.b7df0148.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/WECAKE/static/js/2.b7df0148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3d58b8aa31344748448",
+    "url": "/WECAKE/static/js/3.fc39b07c.chunk.js"
+  },
+  {
+    "revision": "fc5f3d445f36f26ae69b",
+    "url": "/WECAKE/static/js/main.e0735e3e.chunk.js"
+  },
+  {
+    "revision": "8428511c1a93bb15c117",
+    "url": "/WECAKE/static/js/runtime-main.4f8819bd.js"
+  }
+]);
